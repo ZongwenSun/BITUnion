73ecc648b2c84956c38e6fb10b02ba4b7a6dd870
SunZongwen <szongwen@gmail.com>
2015-04-09 23:30:07 +0800

    添加论坛帖子列表界面的部分代码

---
+++ src/cn/edu/bit/bitunion/ForumActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.bit.bitunion;
+
+import android.os.Bundle;
+
+public class ForumActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle bundle) {
+		// TODO Auto-generated method stub
+		super.onCreate(bundle);
+		setContentView(R.layout.activity_forum);
+
+	}
+}
